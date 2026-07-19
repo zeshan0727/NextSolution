dc234b1fa53af18e5ecbcb59516dba921af83ee4
zeshan0727 <Zeshanbarvi@gmail.com>
2026-07-19 11:29:44 +0300

    Add KeychainStore.swift for live market data

---
+++ AITradingDemo/AIScalperDemo/Services/KeychainStore.swift
@@ -0,0 +1,47 @@
+import Foundation
+import Security
+
+enum KeychainStore {
+    private static let service = "uk.zeshanbarvi.aiscalperdemo"
+
+    @discardableResult
+    static func save(_ value: String, account: String) -> Bool {
+        guard let data = value.data(using: .utf8) else { return false }
+        delete(account: account)
+
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: account,
+            kSecAttrAccessible as String: kSecAttrAccessibleAfterFirstUnlockThisDeviceOnly,
+            kSecValueData as String: data
+        ]
+        return SecItemAdd(query as CFDictionary, nil) == errSecSuccess
+    }
+
+    static func read(account: String) -> String? {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: account,
+            kSecReturnData as String: true,
+            kSecMatchLimit as String: kSecMatchLimitOne
+        ]
+
+        var result: AnyObject?
+        guard SecItemCopyMatching(query as CFDictionary, &result) == errSecSuccess,
+              let data = result as? Data else { return nil }
+        return String(data: data, encoding: .utf8)
+    }
+
+    @discardableResult
+    static func delete(account: String) -> Bool {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: account
+        ]
+        let status = SecItemDelete(query as CFDictionary)
+        return status == errSecSuccess || status == errSecItemNotFound
+    }
+}
