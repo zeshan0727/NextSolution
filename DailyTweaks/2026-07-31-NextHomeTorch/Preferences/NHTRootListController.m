59f72480659e4a6b9898fa007b0df2eea79fc75c
zeshan0727 <Zeshanbarvi@gmail.com>
2026-07-31 13:24:22 +0300

    Add direct Settings flashlight test command

--- DailyTweaks/2026-07-31-NextHomeTorch/Preferences/NHTRootListController.m
+++ DailyTweaks/2026-07-31-NextHomeTorch/Preferences/NHTRootListController.m
@@ -1,8 +1,11 @@
 #import "NHTRootListController.h"
 #import <Preferences/PSSpecifier.h>
+#import <UIKit/UIKit.h>
 
 static CFStringRef const NHTPreferenceDomain = CFSTR("com.nextsolution.nexthometorch");
+static CFStringRef const NHTRuntimeDomain = CFSTR("com.nextsolution.nexthometorch.runtime");
 static CFStringRef const NHTPreferencesChanged = CFSTR("com.nextsolution.nexthometorch.preferences.changed");
+static CFStringRef const NHTTestFlashlight = CFSTR("com.nextsolution.nexthometorch.test-flashlight");
 
 @implementation NHTRootListController
 
@@ -25,6 +28,25 @@ static CFStringRef const NHTPreferencesChanged = CFSTR("com.nextsolution.nexthom
     CFPreferencesSetAppValue((__bridge CFStringRef)key, (__bridge CFPropertyListRef)value, NHTPreferenceDomain);
     CFPreferencesAppSynchronize(NHTPreferenceDomain);
     CFNotificationCenterPostNotification(CFNotificationCenterGetDarwinNotifyCenter(), NHTPreferencesChanged, NULL, NULL, true);
+}
+
+- (void)testFlashlight {
+    CFNotificationCenterPostNotification(CFNotificationCenterGetDarwinNotifyCenter(), NHTTestFlashlight, NULL, NULL, true);
+
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        CFPreferencesAppSynchronize(NHTRuntimeDomain);
+        id route = CFBridgingRelease(CFPreferencesCopyAppValue(CFSTR("lastRoute"), NHTRuntimeDomain));
+        id loaded = CFBridgingRelease(CFPreferencesCopyAppValue(CFSTR("tweakLoaded"), NHTRuntimeDomain));
+        NSString *message = [loaded boolValue]
+            ? [NSString stringWithFormat:@"SpringBoard received the command. Route: %@", route ?: @"Waiting for result"]
+            : @"The tweak is not loaded into SpringBoard. Reinstall the correct RootHide or rootless package and respring.";
+
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Flashlight Test"
+                                                                       message:message
+                                                                preferredStyle:UIAlertControllerStyleAlert];
+        [alert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
+        [self presentViewController:alert animated:YES completion:nil];
+    });
 }
 
 @end
