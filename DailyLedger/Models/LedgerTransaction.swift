a7514530931df8caea991b9b625be17c7db37fca
zeshan0727 <Zeshanbarvi@gmail.com>
2026-08-02 16:22:37 +0300

    Add editable chart of accounts report

--- DailyLedger/Models/LedgerTransaction.swift
+++ DailyLedger/Models/LedgerTransaction.swift
@@ -66,6 +66,7 @@ struct LedgerAccount: Identifiable, Codable, Hashable {
     var openingBalance: Decimal
     var isArchived: Bool
     var nature: AccountNature?
+    var chartCode: String?
     let createdAt: Date
 
     init(
@@ -77,6 +78,7 @@ struct LedgerAccount: Identifiable, Codable, Hashable {
         openingBalance: Decimal = 0,
         isArchived: Bool = false,
         nature: AccountNature? = nil,
+        chartCode: String? = nil,
         createdAt: Date = Date()
     ) {
         self.id = id
@@ -87,6 +89,7 @@ struct LedgerAccount: Identifiable, Codable, Hashable {
         self.openingBalance = openingBalance
         self.isArchived = isArchived
         self.nature = nature
+        self.chartCode = chartCode
         self.createdAt = createdAt
     }
 
@@ -311,6 +314,13 @@ struct LedgerSettings: Codable, Equatable {
     var smsImporterLastCheck: Date?
     var smsImporterLastResult: String?
     var expenseBudgets: [ExpenseBudget]
+    var customExpenseCategories: [String]
+    var customIncomeCategories: [String]
+    var hiddenExpenseCategories: [String]
+    var hiddenIncomeCategories: [String]
+    var expenseCategoryCodes: [String: String]
+    var incomeCategoryCodes: [String: String]
+    var chartOfAccountsMigrationVersion: Int
 
     init(
         currencyCode: String = "QAR",
@@ -322,7 +332,14 @@ struct LedgerSettings: Codable, Equatable {
         smsRescanRequestID: Int = 0,
         smsImporterLastCheck: Date? = nil,
         smsImporterLastResult: String? = nil,
-        expenseBudgets: [ExpenseBudget] = []
+        expenseBudgets: [ExpenseBudget] = [],
+        customExpenseCategories: [String] = [],
+        customIncomeCategories: [String] = [],
+        hiddenExpenseCategories: [String] = [],
+        hiddenIncomeCategories: [String] = [],
+        expenseCategoryCodes: [String: String] = LedgerTransaction.defaultExpenseCategoryCodes,
+        incomeCategoryCodes: [String: String] = LedgerTransaction.defaultIncomeCategoryCodes,
+        chartOfAccountsMigrationVersion: Int = 0
     ) {
         self.currencyCode = currencyCode
         self.vendorRules = vendorRules
@@ -334,6 +351,13 @@ struct LedgerSettings: Codable, Equatable {
         self.smsImporterLastCheck = smsImporterLastCheck
         self.smsImporterLastResult = smsImporterLastResult
         self.expenseBudgets = expenseBudgets
+        self.customExpenseCategories = customExpenseCategories
+        self.customIncomeCategories = customIncomeCategories
+        self.hiddenExpenseCategories = hiddenExpenseCategories
+        self.hiddenIncomeCategories = hiddenIncomeCategories
+        self.expenseCategoryCodes = expenseCategoryCodes
+        self.incomeCategoryCodes = incomeCategoryCodes
+        self.chartOfAccountsMigrationVersion = chartOfAccountsMigrationVersion
     }
 
     private enum CodingKeys: String, CodingKey {
@@ -347,6 +371,13 @@ struct LedgerSettings: Codable, Equatable {
         case smsImporterLastCheck
         case smsImporterLastResult
         case expenseBudgets
+        case customExpenseCategories
+        case customIncomeCategories
+        case hiddenExpenseCategories
+        case hiddenIncomeCategories
+        case expenseCategoryCodes
+        case incomeCategoryCodes
+        case chartOfAccountsMigrationVersion
     }
 
     init(from decoder: Decoder) throws {
@@ -362,6 +393,22 @@ struct LedgerSettings: Codable, Equatable {
         smsImporterLastCheck = try values.decodeIfPresent(Date.self, forKey: .smsImporterLastCheck)
         smsImporterLastResult = try values.decodeIfPresent(String.self, forKey: .smsImporterLastResult)
         expenseBudgets = try values.decodeIfPresent([ExpenseBudget].self, forKey: .expenseBudgets) ?? []
+        customExpenseCategories = try values.decodeIfPresent([String].self, forKey: .customExpenseCategories) ?? []
+        customIncomeCategories = try values.decodeIfPresent([String].self, forKey: .customIncomeCategories) ?? []
+        hiddenExpenseCategories = try values.decodeIfPresent([String].self, forKey: .hiddenExpenseCategories) ?? []
+        hiddenIncomeCategories = try values.decodeIfPresent([String].self, forKey: .hiddenIncomeCategories) ?? []
+        expenseCategoryCodes = try values.decodeIfPresent(
+            [String: String].self,
+            forKey: .expenseCategoryCodes
+        ) ?? LedgerTransaction.defaultExpenseCategoryCodes
+        incomeCategoryCodes = try values.decodeIfPresent(
+            [String: String].self,
+            forKey: .incomeCategoryCodes
+        ) ?? LedgerTransaction.defaultIncomeCategoryCodes
+        chartOfAccountsMigrationVersion = try values.decodeIfPresent(
+            Int.self,
+            forKey: .chartOfAccountsMigrationVersion
+        ) ?? 0
     }
 }
 
@@ -421,7 +468,55 @@ struct LedgerData: Codable {
         for index in transactions.indices where transactions[index].vendor?.isEmpty != false {
             transactions[index].vendor = LedgerTransaction.vendorFromMessage(transactions[index].details)
         }
+        applyApprovedChartOfAccountsIfNeeded()
         version = 5
+    }
+
+    private mutating func applyApprovedChartOfAccountsIfNeeded() {
+        guard settings.chartOfAccountsMigrationVersion < 1 else { return }
+        let approved: [UUID: (String, AccountNature)] = [
+            UUID(uuidString: "EA37927C-FEF1-57A0-A0E8-85A6C3334D65")!: ("1010", .asset),
+            UUID(uuidString: "94D9C8CB-A48E-5AF2-AC94-D371CF0FB87A")!: ("1020", .asset),
+            UUID(uuidString: "BF808990-1A06-58F0-AA88-70EFA32B070E")!: ("1030", .asset),
+            UUID(uuidString: "A16378B7-2D98-543B-AB77-3FC888257521")!: ("1040", .asset),
+            UUID(uuidString: "1AE44D3F-1819-5D95-AE1D-572F72E6FA7A")!: ("1050", .asset),
+            UUID(uuidString: "1547562A-85FD-58F0-A988-57B7AAF33D58")!: ("1060", .asset),
+            UUID(uuidString: "49D947CC-0AD0-5E95-ACC6-CB71CF395179")!: ("1070", .asset),
+            UUID(uuidString: "3B390900-F5AF-5233-A217-A416736E7927")!: ("1080", .asset),
+            UUID(uuidString: "1860A963-09C9-5B37-A69B-F262C8FBA024")!: ("1090", .bank),
+            UUID(uuidString: "07111CD1-8254-5C4D-A1D4-FDDF27D820CD")!: ("1100", .bank),
+            UUID(uuidString: "E8027AC1-3E41-5174-A5DF-41DF2B5B90AD")!: ("1110", .bank),
+            UUID(uuidString: "66A2CBBF-C2A5-582F-AE77-CFCB8568ED47")!: ("1120", .asset),
+            UUID(uuidString: "C6E00292-58A8-5514-AC6E-ED767EAA6278")!: ("1130", .asset),
+            UUID(uuidString: "BB424F54-779F-5E74-AA57-17C5DB5E730A")!: ("3010", .control),
+            UUID(uuidString: "85424C31-C710-56D6-AEF0-CB905EDD572F")!: ("3020", .control),
+            UUID(uuidString: "599CA7E4-9B3B-5F42-A3F7-154F5CA83BEB")!: ("2010", .loan),
+            UUID(uuidString: "B5D443C6-A8FE-520B-A087-28AC9D74F855")!: ("2020", .loan),
+            UUID(uuidString: "E4853ECB-D22A-54C5-AC82-D071CEFF8D2D")!: ("2030", .loan),
+            UUID(uuidString: "36AEA784-B9AE-58BA-ACE9-B2DEF19CDD20")!: ("2040", .loan),
+            UUID(uuidString: "518DC96E-D74F-576D-A222-087D3BD8A960")!: ("2050", .loan),
+            UUID(uuidString: "0C1E9E1B-8D13-5E05-A0A4-878B9682499F")!: ("2060", .loan),
+            UUID(uuidString: "9764922E-6AB6-5936-A686-24A127E6B4E0")!: ("2070", .loan),
+            UUID(uuidString: "64B389AB-5782-5E9B-ABDE-125ADCB8B2B5")!: ("2080", .loan),
+            UUID(uuidString: "F8AF715B-7D47-5C37-A4F3-7BE905E1AA22")!: ("2090", .loan),
+            UUID(uuidString: "F08BD228-D21E-5C17-A02A-7D28632AE48A")!: ("2100", .loan),
+            UUID(uuidString: "6F63BFAF-1EC2-52BD-AB84-4BEB58BB3A38")!: ("2110", .loan),
+            UUID(uuidString: "FA78FD34-4F39-536D-A386-58164897E600")!: ("2120", .loan),
+            UUID(uuidString: "151A683D-07BA-5991-A961-D9A090455F01")!: ("2130", .loan),
+            UUID(uuidString: "165F0F18-D2DC-5F46-A65C-71C5E3210E10")!: ("2140", .loan),
+            UUID(uuidString: "58B79BD6-DD56-50A8-A681-757EC4482E53")!: ("2150", .loan),
+            UUID(uuidString: "9CE1D511-38F0-5A9E-AB39-9F0DF8D83E11")!: ("2160", .loan),
+            UUID(uuidString: "E8B095C2-D86C-547E-ACA9-F07F9F088BBF")!: ("2170", .loan),
+            UUID(uuidString: "DC7E4F55-1644-5CBA-ABED-CCACCB08795F")!: ("9010", .asset),
+            UUID(uuidString: "30FEC30E-7CC4-5F1D-A0B2-4426571E5AE5")!: ("9020", .asset),
+            UUID(uuidString: "31FB07F0-1D40-5CD9-AF6A-C8BF7CE150E1")!: ("9030", .asset)
+        ]
+        for index in accounts.indices {
+            guard let entry = approved[accounts[index].id] else { continue }
+            accounts[index].chartCode = entry.0
+            accounts[index].nature = entry.1
+        }
+        settings.chartOfAccountsMigrationVersion = 1
     }
 }
 
@@ -433,5 +528,16 @@ extension LedgerTransaction {
 
     static let incomeCategories = [
         "Salary", "Business", "Refund", "Gift", "Investment", "Other"
+    ]
+
+    static let defaultIncomeCategoryCodes = [
+        "Salary": "4010", "Business": "4020", "Refund": "4030",
+        "Gift": "4040", "Investment": "4050", "Other": "4090"
+    ]
+
+    static let defaultExpenseCategoryCodes = [
+        "Restaurants & Cafes": "5010", "Grocery": "5020", "Shopping": "5030",
+        "Transport": "5040", "Bills": "5050", "Fuel": "5060", "Health": "5070",
+        "Home": "5080", "Family": "5090", "Entertainment": "5100", "Other": "5990"
     ]
 }
