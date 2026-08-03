f033ccc389196825bbbe9452b26206e1ba0ad875
zeshan0727 <Zeshanbarvi@gmail.com>
2026-08-03 06:03:44 +0300

    Add settings controller implementation

---
+++ DailyTweaks/2026-08-03-ScreenRecordingHapticAlert/Preferences/SRHRootListController.m
@@ -0,0 +1,9 @@
+#import "SRHRootListController.h"
+@implementation SRHRootListController
+- (NSArray *)specifiers {
+    if (!_specifiers) {
+        _specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
+    }
+    return _specifiers;
+}
+@end
