f78f05757960546e4a5a19e6e6195aec6eba192c
zeshan0727 <Zeshanbarvi@gmail.com>
2026-08-16 00:02:42 -0700

    Implement category-aware Aura Preferences controller

---
+++ DailyTweaks/2026-08-16-NextAuraCategorySplit/AuraPrefs/AuraPrefs.m
@@ -0,0 +1,150 @@
+#import <UIKit/UIKit.h>
+#import <Preferences/PSListController.h>
+#import <Preferences/PSSpecifier.h>
+#import <CoreFoundation/CoreFoundation.h>
+
+static NSString * const AuraDefaultsDomain = @"com.nextsolution.unlockvibrate";
+static NSString * const AuraChangedNotification = @"com.nextsolution.unlockvibrate/preferences.changed";
+
+static void AuraPost(NSString *name) {
+    if (!name.length) return;
+    CFNotificationCenterPostNotification(CFNotificationCenterGetDarwinNotifyCenter(),
+                                         (__bridge CFStringRef)name,
+                                         NULL, NULL, YES);
+}
+
+static NSString *AuraPlistForLabel(NSString *label) {
+    NSDictionary *map = @{
+        @"Aura Haptics": @"Feedback",
+        @"Aura Thermal": @"ThermalSweat",
+        @"Aura Home": @"HomeScreen",
+        @"Aura Dock": @"DockFolders",
+        @"Aura Lock": @"LockScreen",
+        @"Aura Status": @"StatusBar",
+        @"Aura Control": @"ControlCenter",
+        @"Aura Panel": @"CCSecondPage",
+        @"Aura Modules": @"CCModuleBackgrounds",
+        @"Aura Island": @"DynamicIsland",
+        @"Aura Glow": @"NotificationGlow",
+        @"Aura Player": @"NowPlaying",
+        @"Aura Alerts": @"Notifications",
+        @"Aura Switcher": @"AppSwitcher",
+        @"Aura HUD": @"SystemOverlays",
+        @"Aura Motion": @"Animations",
+        @"Aura Safe": @"SafetyRecovery"
+    };
+    return map[label ?: @""];
+}
+
+@interface AuraCategoryListController : PSListController <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+@property(nonatomic,copy) NSString *pendingModuleSlot;
+@end
+
+@implementation AuraCategoryListController
+
+- (NSArray *)specifiers {
+    if (!_specifiers) {
+        NSString *plistName = [self.specifier propertyForKey:@"plist"];
+        NSString *label = [self.specifier propertyForKey:@"label"] ?: self.specifier.name;
+        if (!plistName.length) plistName = AuraPlistForLabel(label);
+        if (!plistName.length) plistName = @"Feedback";
+        _specifiers = [self loadSpecifiersFromPlistName:plistName target:self];
+        self.title = label.length ? label : @"Aura";
+    }
+    return _specifiers;
+}
+
+- (void)setPreferenceValue:(id)value specifier:(PSSpecifier *)specifier {
+    [super setPreferenceValue:value specifier:specifier];
+    NSString *post = [specifier propertyForKey:@"PostNotification"];
+    if (post.length) AuraPost(post);
+}
+
+- (void)testDynamicIslandSuite:(id)sender { AuraPost(@"com.nextsolution.unlockvibrate/test-dynamic-island-suite"); }
+- (void)testNotificationIsland:(id)sender { AuraPost(@"com.nextsolution.unlockvibrate/test-notification-island"); }
+- (void)testNotificationScreen:(id)sender { AuraPost(@"com.nextsolution.unlockvibrate/test-notification-screen"); }
+- (void)testNotificationAOD:(id)sender { AuraPost(@"com.nextsolution.unlockvibrate/test-notification-aod"); }
+- (void)testNotificationBurning:(id)sender { AuraPost(@"com.nextsolution.unlockvibrate/test-notification-burning"); }
+- (void)refreshBatteryReading:(id)sender { AuraPost(@"com.nextsolution.unlockvibrate/refresh-battery-reading"); }
+- (void)testSweat:(id)sender { AuraPost(@"com.nextsolution.unlockvibrate/test-sweat"); }
+
+- (void)clearSafeSuiteRecovery:(id)sender {
+    NSUserDefaults *d = [[NSUserDefaults alloc] initWithSuiteName:AuraDefaultsDomain];
+    [d removeObjectForKey:@"SafeSuiteBootPending"];
+    [d removeObjectForKey:@"SafeSuiteSafeModeTriggered"];
+    [d synchronize];
+    AuraPost(AuraChangedNotification);
+}
+
+- (void)resetSafeSuiteSettings:(id)sender {
+    NSURL *url = [[NSBundle bundleForClass:self.class] URLForResource:@"SafeLabKeys" withExtension:@"plist"];
+    NSDictionary *dict = url ? [NSDictionary dictionaryWithContentsOfURL:url] : nil;
+    NSArray *keys = [dict[@"keys"] isKindOfClass:NSArray.class] ? dict[@"keys"] : @[];
+    NSUserDefaults *d = [[NSUserDefaults alloc] initWithSuiteName:AuraDefaultsDomain];
+    for (NSString *key in keys) if ([key isKindOfClass:NSString.class]) [d removeObjectForKey:key];
+    [d synchronize];
+    AuraPost(AuraChangedNotification);
+}
+
+- (NSString *)ccBackgroundDirectory {
+    return @"/var/mobile/Library/NextSolutionTweaks/CCBackgrounds";
+}
+
+- (void)configureCCModuleBackground:(PSSpecifier *)specifier {
+    NSString *slot = [specifier propertyForKey:@"moduleSlot"];
+    NSString *title = [specifier propertyForKey:@"moduleTitle"] ?: @"Module";
+    if (!slot.length) return;
+    self.pendingModuleSlot = slot;
+
+    UIAlertController *sheet = [UIAlertController alertControllerWithTitle:title
+                                                                   message:@"Choose a background image or remove the current one."
+                                                            preferredStyle:UIAlertControllerStyleActionSheet];
+    __weak typeof(self) weakSelf = self;
+    [sheet addAction:[UIAlertAction actionWithTitle:@"Choose Photo" style:UIAlertActionStyleDefault handler:^(__unused UIAlertAction *a) {
+        UIImagePickerController *picker = [UIImagePickerController new];
+        picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+        picker.delegate = weakSelf;
+        [weakSelf presentViewController:picker animated:YES completion:nil];
+    }]];
+    [sheet addAction:[UIAlertAction actionWithTitle:@"Remove" style:UIAlertActionStyleDestructive handler:^(__unused UIAlertAction *a) {
+        NSString *dir = [weakSelf ccBackgroundDirectory];
+        NSFileManager *fm = NSFileManager.defaultManager;
+        for (NSString *name in [fm contentsOfDirectoryAtPath:dir error:nil]) {
+            if ([name hasPrefix:[slot stringByAppendingString:@"."]] || [name isEqualToString:slot])
+                [fm removeItemAtPath:[dir stringByAppendingPathComponent:name] error:nil];
+        }
+        AuraPost(AuraChangedNotification);
+    }]];
+    [sheet addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil]];
+    if (sheet.popoverPresentationController) {
+        sheet.popoverPresentationController.sourceView = self.view;
+        sheet.popoverPresentationController.sourceRect = CGRectMake(CGRectGetMidX(self.view.bounds), 40, 1, 1);
+    }
+    [self presentViewController:sheet animated:YES completion:nil];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<UIImagePickerControllerInfoKey,id> *)info {
+    UIImage *image = info[UIImagePickerControllerOriginalImage];
+    NSString *slot = self.pendingModuleSlot;
+    [picker dismissViewControllerAnimated:YES completion:nil];
+    if (!image || !slot.length) return;
+    NSString *dir = [self ccBackgroundDirectory];
+    [NSFileManager.defaultManager createDirectoryAtPath:dir withIntermediateDirectories:YES attributes:nil error:nil];
+    NSData *data = UIImagePNGRepresentation(image);
+    [data writeToFile:[dir stringByAppendingPathComponent:[slot stringByAppendingString:@".png"]] atomically:YES];
+    AuraPost(AuraChangedNotification);
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)resetAllCCModuleBackgrounds:(id)sender {
+    NSString *dir = [self ccBackgroundDirectory];
+    NSFileManager *fm = NSFileManager.defaultManager;
+    for (NSString *name in [fm contentsOfDirectoryAtPath:dir error:nil])
+        [fm removeItemAtPath:[dir stringByAppendingPathComponent:name] error:nil];
+    AuraPost(AuraChangedNotification);
+}
+
+@end
