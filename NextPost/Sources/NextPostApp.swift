62ce8c742872effb5c17967a693d6ba1c91b488b
zeshan0727 <Zeshanbarvi@gmail.com>
2026-08-18 06:27:18 -0700

    Initialize LevelPlay in Next Post

--- NextPost/Sources/NextPostApp.swift
+++ NextPost/Sources/NextPostApp.swift
@@ -2,6 +2,10 @@ import SwiftUI
 
 @main
 struct NextPostApp: App {
+    init() {
+        AdsManager.shared.initializeIfNeeded()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
