f122e99fa11f2f73b29123608d9e1cc8da27b605
zeshan0727 <Zeshanbarvi@gmail.com>
2026-08-18 02:05:03 -0700

    Sanitize legacy settings before ledger store initialization

--- DailyLedger/DailyLedgerApp.swift
+++ DailyLedger/DailyLedgerApp.swift
@@ -2,12 +2,13 @@ import SwiftUI
 
 @main
 struct DailyLedgerApp: App {
-    @StateObject private var store = LedgerStore()
+    @StateObject private var store: LedgerStore
     @Environment(\.scenePhase) private var scenePhase
     @AppStorage("DailyLedgerAppearance") private var appearance = AppAppearance.system.rawValue
 
     init() {
         AppStoreSanitizer.sanitizeLegacySettings()
+        _store = StateObject(wrappedValue: LedgerStore())
         BudgetNotificationService.configure()
     }
 
