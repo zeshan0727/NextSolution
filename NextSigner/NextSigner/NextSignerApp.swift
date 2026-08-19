994f892a0cc962d271873036e1ac4ffa0da6ad3b
zeshan0727 <Zeshanbarvi@gmail.com>
2026-08-19 18:55:20 +0300

    Show live publish log overlay in Next Signer

--- NextSigner/NextSigner/NextSignerApp.swift
+++ NextSigner/NextSigner/NextSignerApp.swift
@@ -2,9 +2,20 @@ import SwiftUI
 
 @main
 struct NextSignerApp: App {
+    @StateObject private var publishLog = PublishLogCenter.shared
+
     var body: some Scene {
         WindowGroup {
             NextSignerRootView()
+                .overlay(alignment: .bottom) {
+                    if publishLog.isVisible {
+                        PublishLogMiniView(center: publishLog)
+                            .padding(.horizontal, 12)
+                            .padding(.bottom, 58)
+                            .transition(.move(edge: .bottom).combined(with: .opacity))
+                    }
+                }
+                .animation(.easeInOut(duration: 0.2), value: publishLog.isVisible)
         }
     }
 }
