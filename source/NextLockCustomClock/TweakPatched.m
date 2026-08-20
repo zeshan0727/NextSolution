72daa2928a1b6713b80c31d6f680cea083df1249
zeshan0727 <Zeshanbarvi@gmail.com>
2026-08-20 13:57:26 +0300

    Fix Test 12 UIFontWeight compile constants

---
+++ source/NextLockCustomClock/TweakPatched.m
@@ -0,0 +1,13 @@
+#import <UIKit/UIKit.h>
+
+#define UIFontWeightUltraLight ((UIFontWeight)-0.8)
+#define UIFontWeightThin       ((UIFontWeight)-0.6)
+#define UIFontWeightLight      ((UIFontWeight)-0.4)
+#define UIFontWeightRegular    ((UIFontWeight)0.0)
+#define UIFontWeightMedium     ((UIFontWeight)0.23)
+#define UIFontWeightSemibold   ((UIFontWeight)0.30)
+#define UIFontWeightBold       ((UIFontWeight)0.40)
+#define UIFontWeightHeavy      ((UIFontWeight)0.56)
+#define UIFontWeightBlack      ((UIFontWeight)0.62)
+
+#include "Tweak.m"
