1a37180506a66ffa43753058375273f5a3c567be
zeshan0727 <Zeshanbarvi@gmail.com>
2026-08-26 12:00:56 +0300

    Update Monetag service worker for current zone

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 11656397
+    "zoneId": 11656484
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
