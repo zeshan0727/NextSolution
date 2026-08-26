e0f51bf00cde249ec4ede7ccf0f0564359fa4ebb
zeshan0727 <Zeshanbarvi@gmail.com>
2026-08-26 13:00:38 +0300

    Update Monetag service worker for nextjailbreak.com

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 11657020
+    "domain": "5gvci.com",
+    "zoneId": 11657052
 }
 self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
